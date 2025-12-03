a8c2dcdc32c7f11a905547c4b16a680cff373804
deepakdev9600 <deepakdev9600@gmail.com>
2025-12-03 16:48:17 +0530

    add side Nav page

---
+++ client/components/Nav.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { Calendar, CalendarDays, LayoutDashboard, User, X } from "lucide-react";
+import Link from "next/link";
+import DropDown from "./reusableComponents/DropDown";
+
+export default function Nav() {
+  return (
+    <div className="min-w-56 h-[93.3vh]  bg-red-300 flex flex-col justify-between p-4">
+      <div>
+        <p>
+          <X size={28} />
+        </p>
+        <nav className="mt-8">
+          <ul className="flex flex-col gap-5">
+            <li>
+              <Link href={"/"} className="flex items-center gap-2 font-light">
+                <LayoutDashboard size={23} /> <span>Dashboard</span>
+              </Link>
+            </li>
+            <li>
+              <DropDown
+                label="Employees"
+                Icon={User}
+                items={[
+                  { label: "All Employees", href: "/employees" },
+                  { label: "Add Employee", href: "/employees/add" },
+                ]}
+              />
+            </li>
+            <li>
+              <DropDown
+                label="Attendance"
+                Icon={CalendarDays}
+                items={[
+                  { label: "Attendance list", href: "/attendance/list" },
+                  { label: "Attendance Report", href: "/attendance/report" },
+                ]}
+              />
+            </li>
+            <li>
+              <DropDown
+                label="Leave"
+                Icon={Calendar}
+                items={[
+                  { label: "Leave list", href: "/leave/list" },
+                  { label: "Approve leave", href: "/leave/approve" },
+                ]}
+              />
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div>
+        <h3>HRMS v-1.0</h3>
+      </div>
+    </div>
+  );
+}
