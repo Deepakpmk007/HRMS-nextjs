9474bd14f822a16ddf0d8e221ff2a444c534967a
deepakdev9600 <deepakdev9600@gmail.com>
2025-12-04 12:04:52 +0530

    Create TableEmployeeList.tsx

---
+++ client/components/Table/TableEmployeeList.tsx
@@ -0,0 +1,24 @@
+export default async function TableEmployeeList() {
+  return (
+    <section>
+      <p>Showing 6 of 6 employees</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Employee Name</th>
+            <th>Employee ID</th>
+            <th>department</th>
+            <th>postion</th>
+            <th>joining data</th>
+            <th>status</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>deepak</td>
+          </tr>
+        </tbody>
+      </table>
+    </section>
+  );
+}
