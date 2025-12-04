ea755d1497c4b72b8737110f5f417870680ba1f5
deepakdev9600 <deepakdev9600@gmail.com>
2025-12-04 12:04:43 +0530

    create search bar in employee page

---
+++ client/components/EmployeeSearchBar.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+
+export default function EmployeeSearchBar({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  const updateFilter = (key: string, value: string) => {
+    const q = new URLSearchParams(params.toString());
+    if (!value.trim()) {
+      q.delete(key);
+    } else {
+      q.set(key, value);
+    }
+    router.push(`?${q.toString()}`);
+  };
+
+  return (
+    <>
+      <form className="flex gap-5">
+        <input
+          type="text"
+          placeholder="Search Name ,Email ........"
+          className="border border-s-stone-500 flex-1 px-2 py-1 outline-none rounded"
+          onChange={(e) => updateFilter("name", e.target.value)}
+        />
+        <div className="flex gap-5">
+          <select
+            id="dept"
+            className="border-s-stone-500 border px-2 outline-none rounded"
+            onChange={(e) => updateFilter("dept", e.target.value)}
+          >
+            <option value="">All Departments</option>
+            <option value="it">IT</option>
+            <option value="hr">HR</option>
+            <option value="finance">Finance</option>
+            <option value="marketing">marketing</option>
+          </select>
+          <select
+            id="status"
+            className="border-s-stone-500 border px-3 outline-none rounded"
+            onChange={(e) => updateFilter("status", e.target.value)}
+          >
+            <option value="">All Status</option>
+            <option value="active">Active</option>
+            <option value="onLeave">On Leave</option>
+            <option value="inActive">In Active</option>
+          </select>
+        </div>
+      </form>
+      <section>{children}</section>
+    </>
+  );
+}
